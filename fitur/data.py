47505e1ecdd387af7bf8985eaf5396f5776b2bc5
Naufal-25 <naufal_2005@student.uns.ac.id>
2025-06-23 14:04:22 +0700

    class update

--- fitur/data.py
+++ fitur/data.py
@@ -1,1 +1,20 @@
+from kelas.butter_cookies import ButterCookies
+from kelas.croissant import Croissant
+from kelas.muffin import Muffin
+from kelas.roti_manis import RotiManis
 
+class Data:
+    def __init__(self):
+        self.produk=[]
+    
+    def tambah(self, kue):
+        self.produk.append(kue)
+
+class Print:
+    def tampil(self):
+        if not self.produk:
+            print ("[[ KOSONG ]]")
+        else:
+            for i, crew in enumerate(self.produk, start=1):
+                print(f"\nProduk {i}:")
+                crew.tampildata()
--- fitur/proses.py
+++ fitur/proses.py
@@ -1,1 +1,8 @@
+from kelas.butter_cookies import ButterCookies
+from kelas.croissant import Croissant
+from kelas.muffin import Muffin
+from kelas.roti_manis import RotiManis
 
+class Profit:
+
+class Proses:
