9b0c33a3457c35db447c469e5e59ac67f261e235
Wava Al <wavaalqudsi@gmail.com>
2025-06-23 13:03:16 +0700

    Produk.py

---
+++ superclass/Produk.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+class Produk(ABC):
+      def __init__(self, nama, kode, bahan, produk_produksi, biaya_produksi, harga):
+        self.nama = nama
+        self.kode = kode
+        self.bahan = bahan
+        self.produk_produksi = produk
+        self.biaya_produksi = biaya
+        self.harga = harga
+
+      @abstractmethod
+      def tampil_data(self): 
+        pass
+
