f3f6c5fa8e82a3144f0a5f49b2c58c416929930d
Wava Al <wavaalqudsi@gmail.com>
2025-06-23 13:10:37 +0700

    Kue Kering.py

---
+++ superclass/Kue Kering.py
@@ -0,0 +1,12 @@
+class KueKering(Produk):
+  def __init__(self, nama, kode, bahan, produk_produksi, biaya_produksi, harga):
+        super().__init__(nama, kode, bahan, produk_produksi, biaya_produksi, harga)
+
+  def tampildata(self):
+        print(f"--- Data Kue Kering ---")
+        print(f"Nama Produk: {self.nama}")
+        print(f"Kode: {self.kode}")
+        print(f"Bahan Utama: {self.bahan}")
+        print(f"Produk Produksi: {self.produk_produksi}")
+        print(f"Biaya Produksi: Rp {self.biaya_produksi:,.2f}")
+        print(f"Harga Jual: Rp {self.harga:,.2f}")
