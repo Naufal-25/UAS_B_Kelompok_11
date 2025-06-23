dc9a32712767c1c27e304a63495328d8d6b81fe0
Naufal-25 <naufal_2005@student.uns.ac.id>
2025-06-23 13:44:31 +0700

    Update butter_cookies.py

--- class/butter_cookies.py
+++ class/butter_cookies.py
@@ -1,10 +1,13 @@
+
+form superclass.kue_kering import KueKering
+from interface.interface import Penopingan
+
 class ButterCookies(KueKering, Penopingan):
-  def __init__(self, nama, kode, bahan, produk_produksi, biaya_produksi, harga):
+    def __init__(self, nama, kode, bahan, produk_produksi, biaya_produksi, harga):
         super().__init__(nama, kode, bahan, produk_produksi, biaya_produksi, harga)
 
-  def adon(self):
+    def adon(self):
         print(f"Adonan '{self.nama}' sedang dibuat.")
-
     def panggang(self):
         print(f"'{self.nama}' sedang dipanggang hingga matang sempurna.")
 
