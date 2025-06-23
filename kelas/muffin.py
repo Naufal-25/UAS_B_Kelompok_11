880e7d5731b3ee83eeab2eea27026ca9426f9eb2
Wava Al <wavaalqudsi@gmail.com>
2025-06-23 20:22:21 +0700

    kelas

--- kelas/muffin.py
+++ kelas/muffin.py
@@ -5,6 +5,15 @@ class Muffin(KueKering, Pengembangan):
     def __init__(self, nama, kode, bahan, produk_produksi, biaya_produksi, harga):
         super().__init__(nama, kode, bahan, produk_produksi, biaya_produksi, harga)
 
+    def tampil_data(self):
+        print(f"--- Data Muffin ---")
+        print(f"Nama Produk: {self.nama}")
+        print(f"Kode: {self.kode}")
+        print(f"Bahan Utama: {self.bahan}")
+        print(f"Produk Produksi: {self.produk_produksi}")
+        print(f"Biaya Produksi: Rp {self.biaya_produksi}")
+        print(f"Harga Jual: Rp {self.harga}")
+
     def adon(self):
         print(f"Adonan '{self.nama}' sedang dicampur.")
 
